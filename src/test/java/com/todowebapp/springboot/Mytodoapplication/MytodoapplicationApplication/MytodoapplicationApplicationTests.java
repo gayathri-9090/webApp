32a649d606705c1386e5d95f1fb7341959dd84df
gayathri <gayathribalasani@gmail.com>
2025-06-28 12:59:46 +0530

    Fix package structure and update test class to resolve compilation error

--- src/test/java/com/todowebapp/springboot/Mytodoapplication/MytodoapplicationApplication/MytodoapplicationApplicationTests.java
+++ src/test/java/com/todowebapp/springboot/Mytodoapplication/MytodoapplicationApplication/MytodoapplicationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.todowebapp.springboot.Mytodoapplication;
+package com.todowebapp.springboot.Mytodoapplication.MytodoapplicationApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
